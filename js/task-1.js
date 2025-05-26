13d5c3a991be47cedeb2e2aeff42628abd6dbb43
Dima_Kravets <dima2.kravets@gmail.com>
2025-05-26 17:50:44 +0300

    HW#8

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const categoryList = document.querySelector("#categories");
-const categoryItem = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categoryList.children.length}`);
-categoryItem.forEach((item) => {
-  console.log(`Category: ${item.querySelector("h2").textContent}`);
-  console.log(`Elements: ${item.querySelectorAll("ul li").length}`);
-});
